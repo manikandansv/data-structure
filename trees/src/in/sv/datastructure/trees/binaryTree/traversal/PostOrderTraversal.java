406cff1feba7395b29f7b2fb1dedc8b7cd43a88d
manikandan.sv <manikandan.sv@accenture.com>
2014-11-24 23:22:26 +0000

    postorder traversal

--- trees/src/in/sv/datastructure/trees/binaryTree/traversal/PostOrderTraversal.java
+++ trees/src/in/sv/datastructure/trees/binaryTree/traversal/PostOrderTraversal.java
@@ -1,5 +1,17 @@
 package in.sv.datastructure.trees.binaryTree.traversal;
 
+import in.sv.datastructure.trees.binaryTree.BinaryTreeNode;
+
 public class PostOrderTraversal {
 
+	public void postOrderRecursive(BinaryTreeNode root){
+		
+		if (root != null){
+			postOrderRecursive(root.getLeft());
+			postOrderRecursive(root.getRight());
+			System.out.println(root.getData());
+		}
+		
+	}
+	
 }
