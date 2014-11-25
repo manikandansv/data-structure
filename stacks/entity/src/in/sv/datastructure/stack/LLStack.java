285fc2bfb2bb5b57ea999a5c1aa1b3a951d4fc89
manikandan.sv <manikandan.sv@accenture.com>
2014-11-25 23:42:39 +0000

    LLStack

--- stacks/entity/src/in/sv/datastructure/stack/LLStack.java
+++ stacks/entity/src/in/sv/datastructure/stack/LLStack.java
@@ -1,5 +1,59 @@
 package in.sv.datastructure.stack;
 
+import java.util.EmptyStackException;
+
+import in.sv.datastructure.linkedlist.ListNode;
+
 public class LLStack {
 
+	private ListNode headNode;
+	
+	public LLStack(){
+		this.headNode = new ListNode(null);
+	}
+	
+	public void push(int data){
+		if (headNode == null){
+			headNode= new ListNode(data); 
+		}
+		else if(headNode.getData() == null){
+			headNode.setData(null);
+		}
+		else{
+			ListNode listNode = new ListNode(data);
+			listNode.setNext(headNode);
+			headNode = listNode;
+		}
+	}
+	
+	public Object top(){
+		if (headNode == null){
+			return null;
+		}else{
+			return headNode.getData();
+		}
+	}
+	
+	public Object pop(){
+		if (headNode == null){
+			throw new EmptyStackException();
+		}else{
+			Object data = headNode.getData();
+			headNode = headNode.getNext();
+			return data;
+		}
+	}
+	
+	public boolean isEmpty(){
+		if (headNode == null){
+			return true;
+		}else{
+			return false;
+		}			
+	}
+	
+	public void deleteStack(){
+		headNode=null;
+	}
+	
 }
